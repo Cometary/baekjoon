af678569bbb094258c947f1ae86b7b2a4daf6ce9
브랜치가머지 <rlagptjd2002@naver.com>
2022-11-17 15:36:43 +0900

    12

--- src/test/java/com/example/demo/Prog1845.java
+++ src/test/java/com/example/demo/Prog1845.java
@@ -1,10 +1,34 @@
 package com.example.demo;
 
+import java.util.HashSet;
+
 public class Prog1845 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//미해결
+	Solution sol = new Solution();
+	int[] numm = new int[]{1, 2, 3, 3};
+	sol.solution(numm);
+	
+	
+	class Solution {
+		public int solution(int[] nums) {
+			int x = nums.length;
+			x = x/2;
+			HashSet<Integer> hashSet = new HashSet<>();
+			for(int num : nums) {
+				hashSet.add(num);
+			}
+			String[] resultArr = hashSet.toArray(new String[0]);
+			
+			int y = resultArr.length;
+			
+			int answer = 0;
+			if(x>y){
+				answer = y;
+			}else{
+				answer = x;
+			}
+			return answer;
+		}
 	}
-
+	
 }
